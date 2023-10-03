4112c6cbceb73e2b1325dd24bbaa4b52813dcbdd
Vitor Cosso <vitorcosso@gmail.com>
2023-10-03 14:58:11 -0300

    Primeiro commit, salvando projeto do zero

---
+++ vittzx/src/main/java/com/vitor/apiEstudo/vittzx/VittzxApplication.java
@@ -0,0 +1,13 @@
+package com.vitor.apiEstudo.vittzx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VittzxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VittzxApplication.class, args);
+	}
+
+}
---
+++ vittzx/src/test/java/com/vitor/apiEstudo/vittzx/VittzxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitor.apiEstudo.vittzx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VittzxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
