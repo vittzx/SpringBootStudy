f154e99b5ae94bc0f6febc209934906837f1a610
Vitor Cosso <vitorcosso@gmail.com>
2023-10-15 17:31:30 -0300

    Adicionnadno Java.util.List

--- api/src/main/java/com/vitor/estudo/api/Controller/MedicoController.java
+++ api/src/main/java/com/vitor/estudo/api/Controller/MedicoController.java
@@ -1,12 +1,16 @@
 package com.vitor.estudo.api.Controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.vitor.estudo.api.Medico.DadosCadastroMedico;
+import com.vitor.estudo.api.Medico.DadosListagemMedico;
 import com.vitor.estudo.api.Medico.Medico;
 import com.vitor.estudo.api.Repository.MedicoRepository;
 
