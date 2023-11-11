40b109e101775a84dfedfdd65aa27676e5940c0c
Vitor Cosso <vitorcosso@gmail.com>
2023-11-11 02:49:12 -0300

    Adicionando Entidade Paciente

---
+++ api/src/main/java/com/vitor/estudo/api/Domain/Paciente/Paciente.java
@@ -0,0 +1,38 @@
+package com.vitor.estudo.api.Domain.Paciente;
+
+import com.vitor.estudo.api.Domain.Endereco.Endereco;
+
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="Paciente")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of="id")
+public class Paciente {
+    
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String email;
+    private String cpf;
+    
+    @Embedded
+    private Endereco endereco;
+
+    private Boolean ativio;
+
+}
